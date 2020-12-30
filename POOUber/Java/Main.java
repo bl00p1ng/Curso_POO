e720656782d92febf0c86fbbf084e470391fec72
bl00p1ng <blooping@protonmail.com>
2020-12-29 21:55:43 -0500

    Accediendo a los atributos de un onjeto en Java usando notación de punto

--- POOUber/Java/Main.java
+++ POOUber/Java/Main.java
@@ -1,7 +1,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");        
+
+        Car car = new Car();
+        car.license = "AMQ258";
+        car.driver = "Andrés López";
+        car.passenger = 4;
+
+        System.out.println("Car License: " + car.license);
+        System.out.println("Car Driver: " + car.driver);
+        System.out.println("Car Passengers: " + car.passenger);        
+
     }
 
 }
