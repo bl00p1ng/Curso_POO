3e4122b2d50408d9c19a2241f97ad46722c86118
bl00p1ng <blooping@protonmail.com>
2020-12-29 22:21:42 -0500

    Instanciando un objeto en Python y accediendo a sus atributos

--- POOUber/Python/main.py
+++ POOUber/Python/main.py
@@ -1,2 +1,8 @@
+from car import Car
+
 if __name__ == "__main__":
-    print("Hello World!")
+    car = Car()
+    car.license = "AMQ678"
+    car.driver = "Felipe López"
+    car.pasengers = 6
+    print(vars(car))
