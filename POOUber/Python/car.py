4f51597cc4ec0b69302779aad4e110cb9a7ded26
bl00p1ng <blooping@protonmail.com>
2020-12-30 19:45:12 -0500

    Declarando un Método Constructor en Python

--- POOUber/Python/car.py
+++ POOUber/Python/car.py
@@ -1,5 +1,11 @@
+from account import Account
+
 class Car:
-    id        = int;
-    license   = str;
-    driver    = str;
-    passenger = int;
+    id        = int
+    license   = str
+    driver    = Account("", "")
+    passenger = int
+
+    def __init__(self, license, driver):
+        self.license = license
+        self.driver  = driver
--- POOUber/Python/main.py
+++ POOUber/Python/main.py
@@ -1,8 +1,7 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
-    car = Car()
-    car.license = "AMQ678"
-    car.driver = "Felipe López"
-    car.pasengers = 6
+    car = Car("ADF875", Account("Felipe López", "GSG458"))       
     print(vars(car))
+    print(vars(car.driver))
